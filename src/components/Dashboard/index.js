1cf55bace9b3228340d001928c54d7fec5d50f96
mallikarjun408 <mallikarjun1988.08@gmail.com>
2022-03-22 22:56:32 -0400

    dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './dashboard.css'
+import headernews from '../../assets/images/headernews.jpg'
 
 export default function Dashboard() {
   return (
     <div>
-     <h1> Test this page </h1> 
+     <header className='header'>
+       <img style={{height:100}} src={headernews} alt="HeaderNews" />
+     </header>
+     <div style={{display:'flex',flexDirection:'row',flex:1}}>
+     <div style={{display:'flex',flexDirection:'column',flex:2, backgroundColor:'#FAF9F6', height:'100vh', borderColor:'red',borderWidth:1}}>
+
+      </div>
+      <div style={{display:'flex',flexDirection:'column', flex:8, backgroundColor:'white', height:'100vh'}}>
+
+     </div>
+     </div>
     </div>
   )
 }
